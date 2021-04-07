4d4629980a04067c4b5bd359fe481f07c2a3d8d2
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-04-07 15:50:16 -0600

    Simplified build process.

--- scripts/build-clojure-nrepl.sh
+++ scripts/build-clojure-nrepl.sh
@@ -1,3 +1,3 @@
 cd nrepl-clojure
 clojure -X:depstar uberjar :jar target/nrepl-clojure.jar
-mvn install:install-file -Dfile=target/nrepl-clojure.jar -DgroupId=ucdenver.ccp -DartifactId=nrepl-clojure -Dversion=0.2.0-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=target/nrepl-clojure.jar -DpomFile=pom.xml
