3226d6ff3617aed2b6f62cd1c260c8814cac27e3
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-04-07 17:12:14 -0600

    Set version to 0.2.0.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 ## build just the plugin and dump it to protege
 rm /opt/protege/plugins/protege-nrepl*.jar
 
-export RELEASE_VERSION=0.2.0-SNAPSHOT
+export RELEASE_VERSION=0.2.0
 
 ./scripts/build-server.sh
 
