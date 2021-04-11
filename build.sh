9e3101bbfd98ff7ea2e61e61638b03ffa5e9eb68
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-04-11 16:52:45 -0600

    Moved removing old plugins to plugin script.

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 ## build just the plugin and dump it to protege
-rm /opt/protege/plugins/protege-nrepl*.jar
 
 export RELEASE_VERSION=0.2.0
 
--- scripts/build-plugin.sh
+++ scripts/build-plugin.sh
@@ -1,1 +1,2 @@
+rm /opt/protege/plugins/protege-nrepl*.jar
 cp plugin/target/protege-nrepl-${RELEASE_VERSION}.jar /opt/protege/plugins/
